23307a638029a8615e870e308d354bd061234c4f
dk734 <deep.kotadia@rutgers.edu>
2017-10-19 19:08:40 -0400

    eclipse project skeleton

---
+++ Chess43/src/chess/Chess.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package chess;
+
+/**
+ * @author dk734
+ *
+ */
+public class Chess {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
