315dd1f4e3575e14786d778cef3cde5bab32511d
Deep Kotadia <deepkotadia@nwk-147-242.nwk.ruw.rutgers.edu>
2017-10-21 16:51:14 -0400

    modified rook to make it work for backward moves and out of board checks

--- Chess43/src/pieces/Rook.java
+++ Chess43/src/pieces/Rook.java
@@ -15,17 +15,24 @@ public class Rook implements Piece {
 
 	@Override
 	public void move(String oldPos, String newPos) {
-		String piece_oldPos=Chess.board.get(oldPos);
-		String piece_newPos=Chess.board.get(newPos);
+		String piece_oldPos = Chess.board.get(oldPos);
+		String piece_newPos = Chess.board.get(newPos);
+		
+		/*to check if newPos is a box in the bounds of the board*/
+		if(Chess.board.containsKey(oldPos) == false) {
+			System.out.println("Illegal move, try again");
+		}
 		
 		/*to check if valid move for a Rook*/
-		if((oldPos.charAt(0)==newPos.charAt(0) || oldPos.charAt(1)==newPos.charAt(1)) && !(oldPos.equals(newPos))) {
+		if((oldPos.charAt(0) == newPos.charAt(0) || oldPos.charAt(1) == newPos.charAt(1)) && !(oldPos.equals(newPos))) {
 			
 			/*to check if newPos is empty*/
 			if(Chess.board.get(newPos).equals("  ") || Chess.board.get(newPos).equals("##")) {
 				if(isPathEmpty(oldPos,newPos)) {
+					//move piece to newPos
 					Chess.board.put(newPos, piece_oldPos);
 					
+					//make oldPos an empty box
 					if(Chess.isBlackBox(oldPos.charAt(0), oldPos.charAt(1))) {
 						Chess.board.put(oldPos, "##");
 					}
@@ -34,15 +41,16 @@ public class Rook implements Piece {
 					}
 				}
 				else {
+					//path not empty, rook can't jump
 					System.out.println("Illegal move, try again");
-					//need to prompt user to dry a different valid move. 
+					//need to prompt user to try a different valid move. 
 				}
-				
 			}
 			
-			/*color case when the newPos is not empty*/
+			/*color case when the newPos is not empty (some piece exists at newPos)*/
 			else {
-				if(piece_oldPos.charAt(0)==piece_newPos.charAt(0)) {
+				
+				if(piece_oldPos.charAt(0) == piece_newPos.charAt(0)) {
 					System.out.println("Illegal move, try again");  //piece color is the same	
 				}
 				else {
@@ -61,7 +69,6 @@ public class Rook implements Piece {
 					else {
 						//path is not empty
 						System.out.println("Illegal move, try again");
-						
 					}
 				}
 				
@@ -77,21 +84,43 @@ public class Rook implements Piece {
 
 	@Override
 	public boolean isPathEmpty(String oldPos, String newPos) {
-		if (oldPos.charAt(0)==newPos.charAt(0)) {
-			int i=oldPos.charAt(1) - '0';
+		if (oldPos.charAt(0) == newPos.charAt(0)) {
+			int i;
+			int numoldPos = oldPos.charAt(1) - '0';
+			int numnewPos = newPos.charAt(1) - '0';
 			
-			for (;i<(newPos.charAt(1)-'0');i++) {
-				if(!(isBoxEmpty(oldPos.charAt(0), i))) {
-					return false;
+			if(numoldPos < numnewPos) { //going forward for white, backward for black
+				for (i = numoldPos+1 ; i < numnewPos ; i++) {
+					if(!(isBoxEmpty(oldPos.charAt(0), i))) {
+						return false;
+					}
+				}
+			}
+			else { //going backward for white, forward for black
+				for (i = numnewPos+1 ; i < numoldPos ; i++) {
+					if(!(isBoxEmpty(oldPos.charAt(0), i))) {
+						return false;
+					}
 				}
 			}
 		}
-		else if(oldPos.charAt(1)==newPos.charAt(1)) {
-			char letter = oldPos.charAt(0);
+		else if(oldPos.charAt(1) == newPos.charAt(1)) {
+			char letter;
+			char letteroldPos = oldPos.charAt(0);
+			char letternewPos = newPos.charAt(0);
 			
-			for (;letter<(newPos.charAt(0));letter++) {
-				if(!(isBoxEmpty(letter, oldPos.charAt(1)-'0'))) {
-					return false;
+			if(letteroldPos < letternewPos) { //going right	
+				for (letter = (char)(letteroldPos+1) ; letter < letternewPos ; letter++) {
+					if(!(isBoxEmpty(letter, oldPos.charAt(1)-'0'))) {
+						return false;
+					}
+				}
+			}
+			else { //going left
+				for (letter = (char)(letternewPos+1) ; letter < letteroldPos ; letter++) {
+					if(!(isBoxEmpty(letter, oldPos.charAt(1)-'0'))) {
+						return false;
+					}
 				}
 			}
 		}
